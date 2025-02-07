adf69adb04d06ceeefb6b34c384cb0e76df75f36
Kimdahee <dh980502@gmail.com>
2025-02-07 15:24:42 +0900

    style : NotosansKR Bold 폰트 ttf 추가,colot styles, text styles 정의

---
+++ lib/ui/color_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorStyles {
+  static const Color black = Color(0xFF000000);
+  static const Color gray1 = Color(0xFF484848);
+  static const Color gray2 = Color(0xFF797979);
+  static const Color gray3 = Color(0xFFA9A9A9);
+  static const Color gray4 = Color(0xFFD9D9D9);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color primary100 = Color(0xFF50744E);
+  static const Color primary80 = Color(0xFF789378);
+  static const Color primary60 = Color(0xFFA3B7A3);
+  static const Color primary40 = Color(0xFFC9D9C9);
+  static const Color primary20 = Color(0xFFE9F1E9);
+
+  static const Color secondary100 = Color(0xFFFF9C00);
+  static const Color secondary80 = Color(0xFFFFA61A);
+  static const Color secondary60 = Color(0xFFFFBA4D);
+  static const Color secondary40 = Color(0xFFFFCE80);
+  static const Color secondary20 = Color(0xFFFFE1B3);
+
+  static const Color rating = Color(0xFFFFAD30);
+
+  static const Color warning1 = Color(0xFFE94A59);
+  static const Color warning2 = Color(0xFFFFE1E7);
+
+  static const Color success = Color(0xFF31B057);
+  static const Color success10 = Color(0xFFEAF7EE);
+
+  static const Color primaryColor = primary100;
+  static const Color secondaryColor = secondary100;
+}
